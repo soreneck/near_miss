35f0b2c8707d2f2c37edca1c94309bbaa1440179
Chu Yeoh <chu_tiong@hotmail.com>
2011-08-06 11:21:26 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NearMiss::Application.config.secret_token = '785fcd523e3eaa51e9d2ee40ebded4e5d4be370b391ba12d1256e5e707f4a346dd686f67e848b4c44f7cb74800e6f657b20b495448151a7570ed56e345f260bd'
